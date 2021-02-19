b3cbe4b24459cd25fac5c63b4c6e9a5c1ca92cbf
DJcri <djcri123@gmail.com>
2021-02-19 11:06:17 -0800

    utilized sw-api to complete character component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,62 @@
-import React from 'react';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+
+import axios from "axios";
+import { BASE_URL } from "./constants";
+
+import Character from "./components/Character";
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
+  // Fetch characters from the API in an effect hook. Remember, anytime you have a
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  const [data, setData] = useState(null);
 
+  //define api request functions
+  const getPlanet = (planetURL, setPlanet) => {
+    axios
+      .get(planetURL)
+      .then((res) => {
+        setPlanet(res.data.name);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const getData = () => {
+    axios
+      .get(`${BASE_URL}people/`)
+      .then((res) => {
+        setData(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  //set data on mount
+  useEffect(getData, []);
+
+  //return nothing if data doesn't exist
+  if (!data) {
+    return null;
+  }
+
+  //if data exists then return app
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      <div className="characters-container">
+        {data.map((v, i) => {
+          return <Character key={i} data={data[i]} getPlanet={getPlanet} />;
+        })}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,54 @@
 // Write your Character component here
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+
+const Character = (props) => {
+  //deconstruct props and define needed variables
+  const { data, getPlanet } = props;
+  const [planet, setPlanet] = useState(null);
+
+  //get planet on mount
+  useEffect(() => {
+    getPlanet(data.homeworld, setPlanet);
+  }, [data, getPlanet]);
+
+  //if planet is still being fetched display loading
+  if (!planet) {
+    return <CharacterCard>Loading</CharacterCard>;
+  }
+
+  //else attach data to card and return it
+  return (
+    <CharacterCard>
+      <h2>{data.name}</h2>
+      <div className="info-container">
+        <Info className="gender">{`Gender: ${data.gender}`}</Info>
+        <Info className="birthyear">{`Birth Year: ${data.birth_year}`}</Info>
+        <Info className="eye-color">{`Eye Color: ${data.eye_color}`}</Info>
+        <Info className="hair-color">{`Hair Color: ${data.hair_color}`}</Info>
+        <Info className="height">{`Height: ${data.height}cm`}</Info>
+        <Info className="homeworld">{`Home World: ${planet}`}</Info>
+        <Info className="mass">{`Mass: ${data.mass}`}</Info>
+        <Info className="skin-color">{`Skin Color: ${data.skin_color}`}</Info>
+      </div>
+    </CharacterCard>
+  );
+};
+
+//export the component
+export default Character;
+
+//component styling
+const CharacterCard = styled.div`
+  margin: 30px 10px;
+  padding: 10px;
+  width: 200px;
+  height: 280px;
+  background: white;
+  box-sizing: border-box;
+`;
+
+const Info = styled.p`
+  margin: 0;
+  text-transform: capitalize;
+`;
